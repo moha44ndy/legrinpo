36e3957707a6306334a1e7487ff6c7283194ec57
mohamed ndiaye <mohamed.ndiaye@edu.ece.fr>
2026-02-22 15:00:35 +0100

    pub

--- app/canaldiscussion/page.tsx
+++ app/canaldiscussion/page.tsx
@@ -300,8 +300,8 @@ export default function CanalDiscussionPage() {
         }
       }
 
-      // Trier par timestamp (plus récent en premier)
-      chats.sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
+      // Trier par timestamp (plus récent en dernier)
+      chats.sort((a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime());
 
       setAllChats(chats);
       
